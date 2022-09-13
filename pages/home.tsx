cf99f2d63f83548cbea546cd40f5c0733acf27d3
ivy <ivy@not-ivy.dev>
2022-09-13 12:20:28 -0700

    add first column

--- pages/home.tsx
+++ pages/home.tsx
@@ -1,5 +1,6 @@
 import Icon from "../components/Icon";
-import { HiOutlineSearch } from 'react-icons/hi';
+import { HiOutlineSearch, HiHome } from 'react-icons/hi';
+import Image from "next/image";
 
 export default function Home() {
   return (
@@ -13,6 +14,22 @@ export default function Home() {
           <HiOutlineSearch className="w-6 h-6 mr-0 md:mr-4" />
           <input className="hidden md:block bg-zinc-600 dark:text-purple-200 dark:caret-purple-200 text-slate-800 outline-none" placeholder="Search"></input>
         </div>
+      </div>
+      <div className="col-span-1 flex flex-col items-end">
+        <Image src='/profile.png' alt='user profile' className="rounded-md" width={100} height={100} />
+        <div className="p-5" />
+        <div className="dark:text-purple-800 text-slate-600 font-bold text-2xl flex flex-col items-end">
+          <button className="dark:text-purple-200 text-slate-800 flex items-center"><HiHome className="m-2" />Home</button>
+          <button>Messages</button>
+          <button>Profile</button>
+          <button>Settings</button>
+        </div>
+      </div>
+      <div className="col-span-1">
+
+      </div>
+      <div className="col-span-1">
+
       </div>
     </div>
   )
