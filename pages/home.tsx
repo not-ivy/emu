a3ededd47bc2aed42644fe9ca9867de634aab1c1
ivy <ivy@not-ivy.dev>
2022-09-15 10:09:03 -0700

    add pagination stuff i guess

--- pages/home.tsx
+++ pages/home.tsx
@@ -1,5 +1,5 @@
 import Icon from "../components/Icon";
-import { HiOutlineSearch, HiHome, HiOutlineChatAlt2, HiOutlineUser, HiOutlineDotsHorizontal } from 'react-icons/hi';
+import { HiOutlineSearch, HiHome, HiOutlineChatAlt2, HiOutlineUser, HiOutlineDotsHorizontal, HiArrowLeft, HiArrowRight } from 'react-icons/hi';
 import Image from "next/image";
 import Head from 'next/head';
 import Link from 'next/link';
@@ -24,8 +24,8 @@ export default function Home() {
           </div>
         </div>
         <div className="col-span-2 flex flex-col items-end border-purple-200 border-r md:border-none">
-          <div className="hidden md:block m-4"><Image src='/profile.png' alt='user profile' className="rounded-md" width={150} height={150} /></div>
-          <div className="text-purple-900 font-bold text-2xl flex flex-col items-end">
+          <div className="hidden md:block"><Image src='/profile.png' alt='user profile' className="rounded-md" width={150} height={150} /></div>
+          <div className="text-purple-400 font-bold text-2xl flex flex-col items-end">
             <button className="text-purple-200 flex items-center"><HiHome className="w-10 h-10 m-2 md:w-8 md:h-8" /><span className="hidden md:block">Home</span></button>
             <button className="flex items-center"><HiOutlineChatAlt2 className="w-10 h-10 m-2 md:w-8 md:h-8" /><span className="hidden md:block">Messages</span></button>
             <button className="flex items-center"><HiOutlineUser className="w-10 h-10 m-2 md:w-8 md:h-8" /><span className="hidden md:block">Profile</span></button>
@@ -37,9 +37,17 @@ export default function Home() {
             I just made this for fun (for now)
           </p>
         </div>
-        <div className="grid grid-flow-row grid-cols-1 gap-y-8 h-screen col-span-5 md:col-span-3  ">
-          <div className="bg-slate-600 rounded-md" />
-          <div className="bg-slate-600 rounded-md" />
+        <div className="col-span-5 md:col-span-3">
+          <div className="flex justify-around items-center mb-4 mx-auto">
+            <HiArrowLeft />
+            <input className="bg-slate-600 text-purple-200 caret-purple-200 outline-none rounded-md p-4 text-center" value='1'></input>
+            <HiArrowRight />
+          </div>
+          <div className="grid grid-flow-row grid-cols-1 gap-y-8 h-screen snap-y overflow-y-scroll">
+            <div className="bg-slate-600 rounded-md" />
+            <div className="bg-slate-600 rounded-md" />
+            <div className="bg-slate-600 rounded-md" />
+          </div>
         </div>
         <div className="col-span-2 bg-slate-600 rounded-md max-h-min p-4 hidden md:block">
           <h3 className="text-xl font-semibold mb-10">Latest News</h3>
