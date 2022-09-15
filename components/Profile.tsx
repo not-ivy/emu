180e8cb44438a286eb060e4fed7e53de00515dc3
ivy <ivy@not-ivy.dev>
2022-09-15 15:29:38 -0700

    add arrow down next to profile

--- components/Profile.tsx
+++ components/Profile.tsx
@@ -1,7 +1,14 @@
 import Image from "next/image";
+import { HiChevronDown } from 'react-icons/hi';
 
 export default function Profile() {
   return (
-    <div className="hidden md:block relative w-10 h-10 m-4"><Image src='/profile.png' alt='user profile' className="rounded-full" layout="fill" objectFit="contain" /></div>
+    <>
+      <div className="hidden md:flex relative w-12 h-12 m-4">
+        <Image src='/profile.png' alt='user profile' className="rounded-md" layout="fill" objectFit="contain" />
+
+      </div>
+      <HiChevronDown className="stroke-slate-600 stroke-2 hover:stroke-slate-400 cursor-pointer" />
+    </>
   )
 }
