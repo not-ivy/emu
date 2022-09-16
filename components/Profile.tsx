baa5e6236133afe3eda6bb2b9120c6feaa41f430
ivy <ivy@not-ivy.dev>
2022-09-15 23:09:22 -0700

    hide down chevron when in mobile

--- components/Profile.tsx
+++ components/Profile.tsx
@@ -6,9 +6,8 @@ export default function Profile() {
     <>
       <div className="hidden md:flex relative w-12 h-12 m-4">
         <Image src='/profile.png' alt='user profile' className="rounded-md" layout="fill" objectFit="contain" />
-
       </div>
-      <HiChevronDown className="stroke-slate-600 stroke-2 hover:stroke-slate-400 cursor-pointer" />
+      <HiChevronDown className="hidden md:flex stroke-slate-600 stroke-2 hover:stroke-slate-400 cursor-pointer" />
     </>
   )
 }
