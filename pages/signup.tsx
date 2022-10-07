1b872e658c7eafe0b2ff574b14c902766881a553
ivy <ivy@not-ivy.dev>
2022-10-07 12:45:34 -0700

    change colors in signup page

--- pages/signup.tsx
+++ pages/signup.tsx
@@ -16,13 +16,13 @@ export default function SignUp() {
       <Head>
         <title>Sign Up</title>
       </Head>
-      <div className="max-w-screen-md mx-auto mt-8 px-16 py-4 border-4 border-slate-500 rounded-md text-purple-200">
-        <div className="flex justify-center items-center"><Icon className="w-8 stroke-purple-200 m-4" /><div className="font-semibold text-2xl my-4 text-center">Sign Up</div></div>
+      <div className="max-w-screen-md mx-auto mt-8 px-16 py-4 border-4 border-hr-b-med rounded-md text-hr-f-high">
+        <div className="flex justify-center items-center"><Icon className="w-8 stroke-hr-f-med m-4" /><div className="font-semibold text-2xl my-4 text-center">Sign Up</div></div>
         <form className="w-full">
           <NewInput placeholder="Username" type="text" ref={usernameRef} />
           <NewInput placeholder="Password" type="password" ref={passwordRef} />
         </form>
-        <button className="transition-colors w-full bg-violet-600 rounded-md p-2 my-8 focus:bg-green-500 focus:text-slate-700 font-semibold text-lg">Submit</button>
+        <button className="transition-colors w-full bg-hr-b-high rounded-md p-2 my-8 focus:bg-green-500 focus:text-slate-700 font-semibold text-lg">Submit</button>
       </div>
     </>
   )
