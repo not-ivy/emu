fc0be79507cd4f3834ae7de19047533247b3477a
ivy <ivy@not-ivy.dev>
2022-10-07 11:59:58 -0700

    add themes script

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,13 +1,15 @@
 import Head from 'next/head'
+import Script from 'next/script'
 import '../styles/globals.css'
 
 function PurotoApp({ Component, pageProps }) {
   return (
     <>
-    <Head>
-      <title>Puroto</title>
-    </Head>
-    <Component {...pageProps} />
+      <Head>
+        <title>Puroto</title>
+      </Head>
+      <Script strategy='worker' src="https://cdn.jsdelivr.net/gh/hundredrabbits/Themes/scripts/lib/theme.js" />
+      <Component {...pageProps} />
     </>
   )
 }
