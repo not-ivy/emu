4e62d860c2ea6fd06b39727db76f7d74af01b48c
ivy <ivy@not-ivy.dev>
2022-10-07 14:40:21 -0700

    change input box color

--- components/NewInput.tsx
+++ components/NewInput.tsx
@@ -3,6 +3,6 @@ import type { MutableRefObject } from "react";
 export default function NewInput(props: { placeholder: string, type: string, ref: MutableRefObject<HTMLInputElement> }) {
   const { placeholder, type, ref } = props;
   return (
-    <input required type={type} ref={ref} className="rounded-sm transition-colors outline-none bg-slate-800 hover:bg-slate-600 my-2 p-4 border-b-2 border-b-slate-500 w-full focus:border-b-white focus:bg-slate-600 invalid:border-b-red-600" placeholder={placeholder} />
+    <input required type={type} ref={ref} className="rounded-sm transition-colors outline-none bg-hr-b-med hover:bg-hr-b-low my-2 p-4 border-b-2 border-b-hr-b-med w-full focus:border-b-hr-b-high focus:bg-hr-b-low invalid:border-b-red-600" placeholder={placeholder} />
   )
 }
