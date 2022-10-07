e64b8ef389f84e06da9f9dbf99253bc65450264b
ivy <ivy@not-ivy.dev>
2022-10-07 14:35:42 -0700

    change search color

--- components/Search.tsx
+++ components/Search.tsx
@@ -30,16 +30,16 @@ export default function Search() {
 
   return (
     <>
-      <div className="hidden md:flex bg-slate-700 items-center p-2 border-slate-600 border-4 rounded-md cursor-pointer" onClick={() => { setVisibility(true) }}>
-        <span className="p-2 w-8 h-8 mx-2 bg-slate-600 rounded-md"><BiCommand /></span>
-        <span className="p-2 w-8 h-8 mx-2 bg-slate-600 rounded-md flex items-center justify-center">K</span>
+      <div className="hidden md:flex bg-hr-b-med items-center p-2 border-hr-b-high border-4 rounded-md cursor-pointer" onClick={() => { setVisibility(true) }}>
+        <span className="p-2 w-8 h-8 mx-2 bg-hr-b-low rounded-md"><BiCommand className="text-hr-f-high" /></span>
+        <span className="p-2 w-8 h-8 mx-2 bg-hr-b-low rounded-md flex items-center justify-center text-hr-f-high">K</span>
       </div>
-      <div className="block md:hidden bg-slate-700 p-4 rounded-md cursor-pointer" onClick={() => { setVisibility(true) }}><HiOutlineSearch className="w-6 h-6 mr-0 md:mr-4 text-purple-200" /></div>
+      <div className="block md:hidden bg-hr-b-med p-4 rounded-md cursor-pointer" onClick={() => { setVisibility(true) }}><HiOutlineSearch className="w-6 h-6 mr-0 md:mr-4 text-hr-f-high" /></div>
       <div className={`${visibility ? 'block' : 'hidden'} w-screen h-screen absolute top-0 left-0 backdrop-brightness-50 backdrop-blur-md bg-opacity-0 z-50`}>
-        <div className="absolute left-2/4 top-1/4 -translate-x-2/4 -translate-y-2/4 bg-slate-600 flex flex-row rounded-lg items-center h-10 p-8">
-          <HiOutlineSearch className="w-6 h-6 mr-0 md:mr-4 text-purple-200 hidden md:block" />
-          <input ref={inputRef} className="bg-slate-600 text-purple-200 caret-purple-200 outline-none" placeholder="Search"></input>
-          <HiX className="cursor-pointer" onClick={() => {inputRef.current.value ? inputRef.current.value = '' : setVisibility(false)}} />
+        <div className="absolute left-2/4 top-1/4 -translate-x-2/4 -translate-y-2/4 bg-hr-b-med flex flex-row rounded-lg items-center h-10 p-8">
+          <HiOutlineSearch className="w-6 h-6 mr-0 md:mr-4 text-hr-f-high hidden md:block" />
+          <input ref={inputRef} className="bg-hr-b-med text-hr-f-high caret-hr-f-high outline-none placeholder:text-hr-f-low" placeholder="Search"></input>
+          <HiX className="cursor-pointer text-hr-f-high w-10 h-10 p-2" onClick={() => {inputRef.current.value ? inputRef.current.value = '' : setVisibility(false)}} />
         </div>
       </div>
     </>
