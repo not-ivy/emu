54c728832638099c81dcb8255cbfc421c5dcb9d0
ivy <ivy@not-ivy.dev>
2022-10-07 11:24:27 -0700

    fix utc time offset
    responsive username page

--- pages/@/[username].tsx
+++ pages/@/[username].tsx
@@ -6,6 +6,7 @@ import { useEffect, useState } from "react";
 import Link from "next/link";
 import { InterfaceUser } from "../../types/db";
 import BlankSlate from "../../components/Blankslate";
+import { DateTime } from "luxon";
 
 export default function User() {
   const [time, setTime] = useState('');
@@ -23,25 +24,39 @@ export default function User() {
 
   useEffect(() => {
     if (!user || user instanceof Error) return;
-    setInterval(() => { setTime(new Date().toLocaleString(navigator.language, { timeZone: user.timezone, timeStyle: 'short' })) }, 1000);
+    const updateTime = () => { setTime(DateTime.now().setZone(user.timezone).setLocale(navigator.language).toLocaleString(DateTime.DATETIME_SHORT)) };
+    updateTime();
+    setInterval(updateTime, 60000);
   }, [user])
 
   if (!user) { return <BlankSlate title="Loading" body="This should not take long..." /> }
   if (user instanceof Error) { return <BlankSlate title="Error" body={user.toString()} /> }
 
   return (
-    <div className="p-8 text-purple-200">
+    <div className="p-4 text-purple-200">
       <TopBar />
-      <div className="flex justify-around items-end pb-4 border-b border-purple-200 max-w-screen-lg mx-auto">
-        <div className="flex items-end">
-          {/* <Image src="/profile.png" alt={`${username}'s profile`} width={128} height={128} className="rounded-md" /> */}
-          <span className="font-semibold text-3xl mx-8">{user.username}</span>
+      <div className="flex justify-around pb-4 border-b border-purple-200 mx-auto flex-col items-start md:flex-row md:items-end">
+        <div className="font-semibold text-3xl w-full text-center md:w-min md:mx-4">
+          {user.username}
         </div>
-        <div className="flex items-center justify-around">
-          <div className="flex items-center mx-4"><HiIdentification aria-label="pronouns" className="h-6 w-6 m-2" />{user.pronouns}</div>
-          <div className="flex items-center mx-4"><HiLocationMarker aria-label="location" className="h-6 w-6 m-2" />{user.location}</div>
-          <div className="flex items-center mx-4"><HiClock aria-label="timezone" className="h-6 w-6 m-2" /><span className="text-slate-500 text-sm ml-2">{time}</span></div>
-          <div className="flex items-center mx-4 cursor-pointer"><HiLink aria-label="website" className="h-6 w-6 m-2" /><Link href={user.website} target="_blank">{user.website}</Link></div>
+        <div className="flex justify-around flex-col items-start md:flex-row md:items-center">
+          <div className="flex items-center md:mx-4">
+            <HiIdentification aria-label="pronouns" className="h-6 w-6 m-2" />
+            {user.pronouns}
+          </div>
+          <div className="flex items-center md:mx-4">
+            <HiLocationMarker aria-label="location" className="h-6 w-6 m-2" />
+            {user.location}
+          </div>
+          <div className="flex items-center md:mx-4">
+            <HiClock aria-label="timezone" className="h-6 w-6 m-2" />
+            UTC {DateTime.now().setZone(user.timezone).offset / 60}:00
+            <span className="text-slate-500 text-xs ml-2">({time})</span>
+          </div>
+          <div className="flex items-center md:mx-4">
+            <HiLink aria-label="website" className="h-6 w-6 m-2" />
+            <Link className="cursor-pointer" href={user.website} target="_blank">{user.website}</Link>
+          </div>
         </div>
       </div>
     </div>
