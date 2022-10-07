d7215bbddfbeb2f47b6c1eed022d9a1bc647984f
ivy <ivy@not-ivy.dev>
2022-10-07 14:27:30 -0700

    more tags for discord embed

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,12 +5,17 @@ export default function Document() {
   return (
     <Html lang="en">
       <meta name="description" content="A future social network for furries." />
-        <Head />
-        <body>
-          <Main />
-          <NextScript />
-          <Script strategy='beforeInteractive' src="https://cdn.jsdelivr.net/gh/hundredrabbits/Themes/scripts/lib/theme.js" />
-        </body>
+      <meta property="og:description" content="A future social network for furries." />
+      <meta name="theme-color" content="#6f50da" />
+      <meta property="og:image" content="/favicon.ico" />
+      <meta property="og:type" content="website" />
+      <meta property="og:site_name" content="Puroto" />
+      <Head />
+      <body>
+        <Main />
+        <NextScript />
+        <Script strategy='beforeInteractive' src="https://cdn.jsdelivr.net/gh/hundredrabbits/Themes/scripts/lib/theme.js" />
+      </body>
     </Html>
   )
 }
