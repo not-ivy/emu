4a29f82e71fa0907a57a9f2ced4adcaaeb22abd1
ivy <ivy@not-ivy.dev>
2022-10-07 12:26:26 -0700

    use theme for index page

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,7 +5,19 @@ module.exports = {
     "./components/**/*.{ts,tsx}",
   ],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        "hr-background": "var(--background)",
+        "hr-f-high": "var(--f_high)",
+        "hr-f-med": "var(--f_med)",
+        "hr-f-low": "var(--f_low)",
+        "hr-f-inv": "var(--f_inv)",
+        "hr-b-high": "var(--b_high)",
+        "hr-b-med": "var(--b_med)",
+        "hr-b-low": "var(--b_low)",
+        "hr-b-inv": "var(--b_inv)",
+      }
+    },
   },
   plugins: [],
 }
