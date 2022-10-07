03158b3e4eecf434b53c9d012ed703212b8bd0f9
ivy <ivy@not-ivy.dev>
2022-10-07 14:36:26 -0700

    change color for profile chevron

--- components/Profile.tsx
+++ components/Profile.tsx
@@ -7,7 +7,7 @@ export default function Profile() {
       <div className="hidden md:flex relative w-12 h-12 m-4">
         <Image src='/profile.png' alt='user profile' className="rounded-md" layout="fill" objectFit="contain" />
       </div>
-      <HiChevronDown className="hidden md:flex stroke-slate-600 stroke-2 hover:stroke-slate-400 cursor-pointer" />
+      <HiChevronDown className="hidden md:flex stroke-hr-f-med stroke-2 hover:stroke-hr-f-high cursor-pointer" />
     </>
   )
 }
