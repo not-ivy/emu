59ecdab44e74de738baf41dd65af42861cc3e9d0
ivy <ivy@not-ivy.dev>
2022-10-07 14:30:01 -0700

    change tabs color

--- components/Tabs.tsx
+++ components/Tabs.tsx
@@ -6,20 +6,20 @@ export default function Tabs() {
   const setSelectedPanel = (selectedPanel: Panels) => { usePanel.setState({ selectedPanel }) }
 
   return (
-    <div className="text-purple-400 font-bold text-2xl flex flex-col items-end">
-      <button aria-label='home' className={`${selected === 'home' ? 'text-purple-200' : ''} flex items-center`} onClick={() => {setSelectedPanel('home')}}>
+    <div className="text-hr-f-med font-bold text-2xl flex flex-col items-end">
+      <button aria-label='home' className={`${selected === 'home' ? 'text-hr-f-high' : ''} flex items-center`} onClick={() => {setSelectedPanel('home')}}>
         {selected === 'home' ? <HiHome className="w-10 h-10 m-2 md:w-8 md:h-8" /> : <HiOutlineHome className='w-10 h-10 m-2 md:w-8 md:h-8' />}
         <span className="hidden md:block">Home</span>
       </button>
-      <button aria-label='messages' className={`${selected === 'messages' ? 'text-purple-200' : ''} flex items-center`} onClick={() => {setSelectedPanel('messages')}}>
+      <button aria-label='messages' className={`${selected === 'messages' ? 'text-hr-f-high' : ''} flex items-center`} onClick={() => {setSelectedPanel('messages')}}>
         {selected === 'messages' ? <HiChatAlt2 className="w-10 h-10 m-2 md:w-8 md:h-8" /> : <HiOutlineChatAlt2 className="w-10 h-10 m-2 md:w-8 md:h-8" />}
         <span className="hidden md:block">Messages</span>
       </button>
-      <button aria-label='profile' className={`${selected === 'profile' ? 'text-purple-200' : ''} flex items-center`} onClick={() => {setSelectedPanel('profile')}}>
+      <button aria-label='profile' className={`${selected === 'profile' ? 'text-hr-f-high' : ''} flex items-center`} onClick={() => {setSelectedPanel('profile')}}>
         {selected === 'profile' ? <HiUser className="w-10 h-10 m-2 md:w-8 md:h-8" /> : <HiOutlineUser className="w-10 h-10 m-2 md:w-8 md:h-8" />}
         <span className="hidden md:block">Profile</span>
       </button>
-      <button aria-label='more' className={`${selected === 'more' ? 'text-purple-200' : ''} flex items-center`} onClick={() => {setSelectedPanel('more')}}>
+      <button aria-label='more' className={`${selected === 'more' ? 'text-hr-f-high' : ''} flex items-center`} onClick={() => {setSelectedPanel('more')}}>
         <HiOutlineDotsHorizontal className="w-10 h-10 m-2 md:w-8 md:h-8" />
         <span className="hidden md:block">More</span></button>
     </div>
