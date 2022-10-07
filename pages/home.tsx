671ddf785151bb9215871717f1592f611ceb4f70
ivy <ivy@not-ivy.dev>
2022-10-07 14:42:33 -0700

    fix remaining colors

--- pages/home.tsx
+++ pages/home.tsx
@@ -26,19 +26,19 @@ export default function Home() {
         </div>
         <div className="flex justify-around items-center mt-4 mx-auto">
           <HiArrowLeft />
-          <input className="bg-hr-b-high text-hr-f-high caret-hr-f-high outline-none rounded-md p-4 text-center" placeholder='1'></input>
+          <input className="bg-hr-b-med text-hr-f-high caret-hr-f-high outline-none rounded-md p-4 text-center placeholder:text-hr-f-med" placeholder='1'></input>
           <HiArrowRight />
         </div>
       </main>
-      <div className="col-span-2 bg-hr-b-high rounded-md h-1/4 p-4 hidden md:block">
+      <div className="col-span-2 bg-hr-b-med rounded-md h-1/4 p-4 hidden md:block">
         <h3 className="text-xl font-semibold mb-10">Latest News</h3>
         <div className="flex flex-col">
           <div className="grid grid-cols-4 h-20">
-            <div className="hidden md:block bg-hr-b-med rounded-md col-span-1 mr-4"></div>
+            <div className="hidden md:block bg-hr-b-high rounded-md col-span-1 mr-4"></div>
             <div className="col-span-3 flex flex-col justify-between">
-              <div className="h-7 rounded-lg bg-hr-b-med w-full"></div>
-              <div className="h-4 rounded-lg bg-hr-b-med w-3/4"></div>
-              <div className="h-4 rounded-lg bg-hr-b-med w-1/3"></div>
+              <div className="h-7 rounded-lg bg-hr-b-high w-full"></div>
+              <div className="h-4 rounded-lg bg-hr-b-high w-3/4"></div>
+              <div className="h-4 rounded-lg bg-hr-b-high w-1/3"></div>
             </div>
           </div>
         </div>
