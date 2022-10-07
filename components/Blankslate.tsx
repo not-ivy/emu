93620721091dd885c74e29e48ae17581ab96ea7c
ivy <ivy@not-ivy.dev>
2022-10-07 12:44:22 -0700

    change blankslate colors

--- components/Blankslate.tsx
+++ components/Blankslate.tsx
@@ -4,12 +4,12 @@ export default function BlankSlate(props: { title: string, body: string }) {
   const { title, body } = props;
   return (
     <div className="w-full h-screen flex items-center justify-center">
-      <div className="w-full text-purple-200 max-w-screen-sm">
+      <div className="w-full text-hr-f-high max-w-screen-sm">
         <h3 className="text-center m-4 text-3xl font-medium">{title}</h3>
         <p className="text-center">{body}</p>
-        <hr className="border-slate-500 border-2 w-full mx-auto m-4 expand" />
+        <hr className="border-hr-b-med border-2 w-full mx-auto m-4 expand" />
         <div className="flex justify-center items-center">
-          <Icon className="w-8 h-8 m-4 stroke-purple-200" />
+          <Icon className="w-8 h-8 m-4 stroke-hr-f-med" />
           <span className="font-semibold">Puroto</span>
         </div>
       </div>
