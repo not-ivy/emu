e19f7b2399fd631de82b5a5d3797d6ecfcb93e99
ivy <ivy@not-ivy.dev>
2022-10-07 12:26:15 -0700

    fix script load

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,14 +1,20 @@
 import Head from 'next/head'
 import Script from 'next/script'
+import { useEffect } from 'react'
 import '../styles/globals.css'
 
 function PurotoApp({ Component, pageProps }) {
+  useEffect(() => {
+    const theme = new (window as any).Theme();
+    theme.install(document.body);
+    theme.start();
+  })
   return (
     <>
       <Head>
         <title>Puroto</title>
       </Head>
-      <Script strategy='worker' src="https://cdn.jsdelivr.net/gh/hundredrabbits/Themes/scripts/lib/theme.js" />
+      <Script strategy='beforeInteractive' src="https://cdn.jsdelivr.net/gh/hundredrabbits/Themes/scripts/lib/theme.js" />
       <Component {...pageProps} />
     </>
   )
