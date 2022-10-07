f1556e6851a97874bc15038baba8218f170f122c
ivy <ivy@not-ivy.dev>
2022-10-07 14:23:45 -0700

    add lang and description for seo(:sick:)

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -3,13 +3,14 @@ import Script from 'next/script'
 
 export default function Document() {
   return (
-    <Html>
-      <Head />
-      <body>
-        <Main />
-        <NextScript />
-        <Script strategy='beforeInteractive' src="https://cdn.jsdelivr.net/gh/hundredrabbits/Themes/scripts/lib/theme.js" />
-      </body>
+    <Html lang="en">
+      <meta name="description" content="A future social network for furries." />
+        <Head />
+        <body>
+          <Main />
+          <NextScript />
+          <Script strategy='beforeInteractive' src="https://cdn.jsdelivr.net/gh/hundredrabbits/Themes/scripts/lib/theme.js" />
+        </body>
     </Html>
   )
 }
